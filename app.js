b66094f9ced6932111988fb99404f3550955134e
benitangela <benitalim21@gmail.com>
2023-05-25 13:56:44 +0900

    Update all functionality with mongoose

--- app.js
+++ app.js
@@ -1,48 +1,141 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
-
-const items = ["Buy Food", "Cook food", "Eat food"];
-const workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  let day = date.getDate();
-  res.render("list", {listTitle: day, newListItems: items});
-})
+// Implement database
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true});
 
-app.get("/work", function(req, res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-})
+const itemsSchema = {
+  name: String
+};
+const Item = mongoose.model("Item", itemsSchema);
+
+const rubbish = new Item({
+  name: "Throw rubbish"
+});
+const clean = new Item({
+  name: "Clean house"
+});
+const grocery = new Item({
+  name: "Do groceries"
+});
+
+// insert data to the collection
+const defaultItems = [rubbish, clean, grocery];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+const List = mongoose.model("List", listSchema);
+
+// Item.deleteOne({name: "Do groceries"}).then(function(){
+//   mongoose.connection.close();
+//   console.log("Successfully delete item.");
+// }).catch(function(error){
+//   console.log(error);
+// });
+
+app.get("/", function(req, res){
+  // log all items in the collection
+  Item.find().then(function(foundItems){
+    // Check items availability in collection
+    if (foundItems.length === 0){
+      Item.insertMany(defaultItems).then(function(){
+        console.log("Successfully insert default items.");
+      }).catch(function(error){
+        console.log(error);
+      });
+      res.redirect("/");
+    }else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
+
+});
+
+app.get("/:customListName", function(req, res){
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}).then(function(foundList){
+    if(!foundList){
+      // Create new list
+      const list = new List({
+        name: customListName,
+        items: defaultItems
+      });
+
+      list.save();
+      res.redirect("/"+customListName);
+    } else {
+      // Show existing list
+      res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+    }
+  }).catch(function(error){
+    console.log(error);
+  });
+
+
+});
 
 app.get("/about", function(req, res){
   res.render("about");
-})
+});
 
 app.post("/", function(req, res){
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-  if(req.body.list === "Work"){
-    workItems.push(item);
-    res.redirect("/work");
-  } else{
-    items.push(item);
+  const item = new Item({
+    name: itemName
+  });
+
+  if(listName === "Today"){
+    // insert new item
+    item.save();
     res.redirect("/");
+  } else{
+    List.findOne({name: listName}).then(function(foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
   }
-})
+
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkedbox;
+  const listName = req.body.listName;
+
+  if(listName === "Today"){
+    // Delete finished item(s)
+    Item.findByIdAndRemove(checkedItemId).then(function(){
+      console.log("Successfully delete finished item.");
+      res.redirect("/");
+    });
+  } else {
+    List.findOneAndUpdate({name: listName},{$pull: {items: {_id: checkedItemId}}}).then(function(){
+      res.redirect("/" + listName);
+    });
+  }
+
+});
 
 app.post("/work", function(req, res) {
   const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
-})
+});
 
 app.listen(3000, function(){
   console.log("Server running from server 3000.");
-})
+});
